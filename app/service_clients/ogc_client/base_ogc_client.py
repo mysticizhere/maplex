8bf1160c77aedf43c48d755185417f1434de535e
Bhanu Pratap <76814409+mysticizhere@users.noreply.github.com>
2026-02-21 21:10:15 +0530

    headers and auth token

--- app/service_clients/ogc_client/base_ogc_client.py
+++ app/service_clients/ogc_client/base_ogc_client.py
@@ -4,6 +4,13 @@ from app.service_clients import BaseAPIClient
 
 
 class BaseOgcClient(BaseAPIClient):
-
     _host = CONFIG.config["OGC"]["HOST"]
     _timeout = CONFIG.config["OGC"]["TIMEOUT"]
+
+    @classmethod
+    def prepare_headers(cls, **extra):
+        headers = super().prepare_headers(**extra)
+        auth_token = CONFIG.config.get("OGC", {}).get("AUTH_TOKEN")
+        if auth_token:
+            headers["Authorization"] = f"Bearer {auth_token}"
+        return headers
